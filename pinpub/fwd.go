5f05b523584dbffca2f354f6e7ea26dbc51c77ad
David Ramon Prados <davidramon3@hotmail.es>
2020-08-13 20:54:19 -0400

    Clean up hackathon

--- pinpub/fwd.go
+++ pinpub/fwd.go
@@ -6,12 +6,14 @@ import (
 	"strings"
 )
 
+// getCidFromPath returns the CID for the given path
 func getCidFromPath(s string) string {
 	ss := strings.Split(s, "/")
 	return ss[len(ss)-1]
 
 }
 
+// getIPprefix returns the IP for the gateway
 func getIPprefix(s string) string {
 	ip4Prefixes := strings.Split(s, ".")
 	if len(ip4Prefixes) == 4 {
@@ -21,6 +23,7 @@ func getIPprefix(s string) string {
 	return ip6Prefix
 }
 
+// GetFwderHandler serves the CID content with the appropiate gateway redirection
 func GetFwderHandler(cfg *config, cid2topic map[string]string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cid := getCidFromPath(r.URL.Path[1:])
@@ -41,6 +44,7 @@ func GetFwderHandler(cfg *config, cid2topic map[string]string) func(w http.Respo
 	}
 }
 
+// GetDebugHandler it displays the redirection logic for debug/trace purposes
 func GetDebugHandler(cfg *config, cid2topic map[string]string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ip: %v\n", r.RemoteAddr)
@@ -67,6 +71,7 @@ func GetDebugHandler(cfg *config, cid2topic map[string]string) func(w http.Respo
 	}
 }
 
+// makeFwdPath returns the URL for the CID and the selected Gateway
 func makeFwdPath(gatewayID, cid string) string {
 	return fmt.Sprintf("http://%s/gateway/%s", gatewayID, cid)
 }
